d83207ff09015c77582bc4f6ade099d30f8e06d3
Felipe Costa <33869171+felipecosta09@users.noreply.github.com>
2023-10-27 19:10:56 -0500

    test

--- ecs/variables.tf
+++ ecs/variables.tf
@@ -8,7 +8,7 @@ variable "aws_region" {
 variable "image_uri" {
   description = "URI of the docker image"
   type        = string
-  default     = "563763293556.dkr.ecr.us-east-1.amazonaws.com/mariogame:latest"
+  default     = ""
 }
 
 variable "prefix" {
