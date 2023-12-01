4a347edac2f858336823621090717563625ffd20
Felipe Costa <33869171+felipecosta09@users.noreply.github.com>
2023-12-01 11:22:05 -0600

    Update eks.tf

--- k8s/eks.tf
+++ k8s/eks.tf
@@ -28,7 +28,7 @@ module "eks" {
       instance_types = ["t3.medium"]
       disk_size       = 20
       min_size     = 1
-      max_size     = 1
+      max_size     = 2
       desired_size = 1
       tags = {
         Environment = var.environment
