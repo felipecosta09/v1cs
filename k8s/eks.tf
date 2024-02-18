004f8e210a3f5f32639753b80849d15143c7b9b6
Felipe Costa <33869171+felipecosta09@users.noreply.github.com>
2024-02-18 21:59:11 +0400

    Update eks.tf

--- k8s/eks.tf
+++ k8s/eks.tf
@@ -8,7 +8,7 @@ module "eks" {
   version = "19.17.2"
 
   cluster_name    = local.cluster_name
-  cluster_version = "1.28"
+  cluster_version = "1.29"
   
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
