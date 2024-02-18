1458aed857ebbab4e2bb114b93f505146fad88c1
Felipe Costa <33869171+felipecosta09@users.noreply.github.com>
2024-02-18 23:24:51 +0400

    Update providers.tf

--- ecs/providers.tf
+++ ecs/providers.tf
@@ -7,7 +7,7 @@ terraform {
   }
   required_version = ">= 0.15.0"
     backend "s3" {
-    bucket = "felipe-state-files"
+    bucket = "felipec-state-files"
     key    = "v1cs/ecs/terraform.tfstate"
     region = "us-east-1"
     }
