4cb533a8e6f57aca93679a495d16b33dad738df1
Felipe Costa <33869171+felipecosta09@users.noreply.github.com>
2024-03-12 09:32:51 +0400

    Update eks.tf

--- k8s/eks.tf
+++ k8s/eks.tf
@@ -1,38 +1,79 @@
+data "aws_availability_zones" "available" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
 
 locals {
-  cluster_name = "${var.prefix}-eks-cluster-${random_string.suffix.result}"
+  cluster_name = "${var.prefix}-eks-${random_string.suffix.result}"
+  vpc_name      = "${var.prefix}-vpc-${random_string.suffix.result}"
+  node_group_name = "${var.prefix}-node-${random_string.suffix.result}"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.0.0"
+
+  name = local.vpc_name
+  tags = {
+    Name        = local.vpc_name
+    Environment = var.environment
+  }
+  cidr = "10.0.0.0/16"
+  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = 1
+  }
 }
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "19.17.2"
+  version = "19.15.3"
 
   cluster_name    = local.cluster_name
   cluster_version = "1.29"
-  
+  tags = {
+    Name        = local.cluster_name
+    Environment = var.environment
+  }
+
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
   cluster_endpoint_public_access = true
-  cluster_endpoint_private_access = true
 
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
 
   }
-  iam_role_additional_policies = {
-        additional = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-      }
+
   eks_managed_node_groups = {
-    node = {
-      name = "${var.prefix}-nodes"
+    one = {
+      name = local.node_group_name
+
       instance_types = ["t3.small"]
-      disk_size       = 20
+
       min_size     = 1
-      max_size     = 2
-      desired_size = 1
+      max_size     = 3
+      desired_size = 2
       tags = {
+        Name = local.node_group_name
         Environment = var.environment
       }
     }
-}
+  }
 }
