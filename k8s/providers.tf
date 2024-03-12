8fa55942fef5e6d0018009baf7d116a7e2ab04af
Felipe Costa <33869171+felipecosta09@users.noreply.github.com>
2024-03-12 09:31:51 +0400

    Update providers.tf

--- k8s/providers.tf
+++ k8s/providers.tf
@@ -2,15 +2,15 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.23.1"
+      version = "5.40.0"
     }
     random = {
       source = "hashicorp/random"
-      version = "3.5.1"
+      version = "3.6.0"
     }
     null = {
       source = "hashicorp/null"
-      version = "3.2.1"
+      version = "3.2.2"
     }
   }
   required_version = ">= 0.15.0"
